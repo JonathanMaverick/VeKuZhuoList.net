44be727b50b939a7116b1a00aa9ae7395161d347
Jonathan Maverick <jonathanmaverick.jm@gmail.com>
2024-07-16 23:56:24 +0700

    storage implementation

--- database/seeders/AnimeSeeder.php
+++ database/seeders/AnimeSeeder.php
@@ -25,7 +25,7 @@ class AnimeSeeder extends Seeder
                 'views' => 1000000,
                 'mal_score' => 8.27,
                 'synopsis' => 'Naruto Uzumaki, a mischievous adolescent ninja, struggles as he searches for recognition and dreams of becoming the Hokage, the village\'s leader and strongest ninja.',
-                'trailer_url' => 'https://www.youtube.com/watch?v=QczGoCmX-pI',
+                'trailer_url' => 'QczGoCmX-pI',
                 'total_episodes' => 500,
                 'image_url' => 'anime_images/naruto.png',
                 'studio_id' => 15,
