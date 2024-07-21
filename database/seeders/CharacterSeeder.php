772fcb81d7e2aed23d4a4aca9de188359469067b
Jonathan Maverick <jonathanmaverick.jm@gmail.com>
2024-07-21 20:43:11 +0700

    seeding

--- database/seeders/CharacterSeeder.php
+++ database/seeders/CharacterSeeder.php
@@ -39,6 +39,60 @@ class CharacterSeeder extends Seeder
                 'character_image' => 'https://cdn.myanimelist.net/images/characters/9/69275.jpg',
                 'anime_id' => 1,
                 'voice_actor_id' => 4
+            ],
+            [
+                'character_name' => 'Yuuji Itadori',
+                'character_image' => 'https://cdn.myanimelist.net/images/characters/6/467646.jpg',
+                'anime_id' => 2,
+                'voice_actor_id' => 5
+            ],
+            [
+                'character_name' => 'Megumi Fushiguro',
+                'character_image' => 'https://cdn.myanimelist.net/images/characters/2/392689.jpg',
+                'anime_id' => 2,
+                'voice_actor_id' => 6
+            ],
+            [
+                'character_name' => 'Nobara Kugisaki',
+                'character_image' => 'https://cdn.myanimelist.net/images/characters/12/422313.jpg',
+                'anime_id' => 2,
+                'voice_actor_id' => 7
+            ],
+            [
+                'character_name' => 'Gojo Satoru',
+                'character_image' => 'https://cdn.myanimelist.net/images/characters/15/422168.jpg',
+                'anime_id' => 2,
+                'voice_actor_id' => 8
+            ],
+            [
+                'character_name' => 'Eren Yeager',
+                'character_image' => 'https://cdn.myanimelist.net/images/characters/10/216895.jpg',
+                'anime_id' => 3,
+                'voice_actor_id' => 9
+            ],
+            [
+                'character_name' => 'Mikasa Ackerman',
+                'character_image' => 'https://cdn.myanimelist.net/images/characters/9/215563.jpg',
+                'anime_id' => 3,
+                'voice_actor_id' => 10
+            ],
+            [
+                'character_name' => 'Armin Arlert',
+                'character_image' => 'https://cdn.myanimelist.net/images/characters/8/220267.jpg',
+                'anime_id' => 3,
+                'voice_actor_id' => 11
+            ],
+            [
+                'character_name' => 'Power',
+                'character_image' => 'https://cdn.myanimelist.net/images/characters/7/494969.jpg',
+                'anime_id' => 4,
+                'voice_actor_id' => 12
+            ],
+            [
+                'character_name' => 'Denji',
+                'character_image' => 'https://cdn.myanimelist.net/images/characters/3/492407.jpg',
+                'anime_id' => 4,
+                'voice_actor_id' => 13
             ]
         ];
 
--- database/seeders/VoiceActorSeeder.php
+++ database/seeders/VoiceActorSeeder.php
@@ -16,24 +16,62 @@ class VoiceActorSeeder extends Seeder
     public function run()
     {
         //
-        VoiceActor::create([
-            'voice_actor_name' => 'Junko Takeuchi',
-            'voice_actor_image' => 'https://cdn.myanimelist.net/images/voiceactors/1/54677.jpg'
-        ]);
+        $voice_actor = [
+            [
+                'voice_actor_name' => 'Junko Takeuchi',
+                'voice_actor_image' => 'https://cdn.myanimelist.net/images/voiceactors/1/54677.jpg'
+            ],
+            [
+                'voice_actor_name' => 'Kazuhiko Inoue',
+                'voice_actor_image' => 'https://cdn.myanimelist.net/images/voiceactors/3/68015.jpg'
+            ],
+            [
+                'voice_actor_name' => 'Noriaki Sugiyama',
+                'voice_actor_image' => 'https://cdn.myanimelist.net/images/voiceactors/3/73289.jpg'
+            ],
+            [
+                'voice_actor_name' => 'Chie Nakamura',
+                'voice_actor_image' => 'https://cdn.myanimelist.net/images/voiceactors/2/75895.jpg'
+            ],
+            [
+                'voice_actor_name' => 'Junya Enoki',
+                'voice_actor_image' => 'https://cdn.myanimelist.net/images/voiceactors/2/62840.jpg'
 
-        VoiceActor::create([
-            'voice_actor_name' => 'Kazuhiko Inoue',
-            'voice_actor_image' => 'https://cdn.myanimelist.net/images/voiceactors/3/68015.jpg'
-        ]);
+            ],
+            [
+                'voice_actor_name' => 'Yuma Uchida',
+                'voice_actor_image' => 'https://cdn.myanimelist.net/images/voiceactors/2/73526.jpg'
+            ],
+            [
+                'voice_actor_name' => 'Asami Seto',
+                'voice_actor_image' => 'https://cdn.myanimelist.net/images/voiceactors/3/65839.jpg'
+            ],
+            [
+                'voice_actor_name' => 'Yuuichi Nakamura',
+                'voice_actor_image' => 'https://cdn.myanimelist.net/images/voiceactors/1/74056.jpg'
+            ],
+            [
+                'voice_actor_name' => 'Yuuki Kaji',
+                'voice_actor_image' => 'https://cdn.myanimelist.net/images/voiceactors/2/66416.jpg'
+            ],
+            [
+                'voice_actor_name' => 'Yui Ishikawa',
+                'voice_actor_image' => 'https://cdn.myanimelist.net/images/voiceactors/2/69967.jpg'
+            ],
+            [
+                'voice_actor_name' => 'Marina Inoue',
+                'voice_actor_image' => 'https://cdn.myanimelist.net/images/voiceactors/1/68016.jpg'
+            ],
+            [
+                'voice_actor_name' => 'Faizou Ai',
+                'voice_actor_image' => 'https://cdn.myanimelist.net/images/voiceactors/2/76160.jpg'
+            ],
+            [
+                'voice_actor_name' => 'Kikunosuke Toya',
+                'voice_actor_image' => 'https://cdn.myanimelist.net/images/characters/3/492407.jpg'
+            ]
+        ];
 
-        VoiceActor::create([
-            'voice_actor_name' => 'Noriaki Sugiyama',
-            'voice_actor_image' => 'https://cdn.myanimelist.net/images/voiceactors/3/73289.jpg'
-        ]);
-
-        VoiceActor::create([
-            'voice_actor_name' => 'Chie Nakamura',
-            'voice_actor_image' => 'https://cdn.myanimelist.net/images/voiceactors/2/75895.jpg'
-        ]);
+        VoiceActor::insert($voice_actor);
     }
 }
