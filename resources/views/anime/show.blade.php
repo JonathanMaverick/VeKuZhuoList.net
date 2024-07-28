a8b3908a27985546d1f29852b25faf5ec8a1b6b9
Jonathan Maverick <jonathanmaverick.jm@gmail.com>
2024-07-28 16:49:22 +0700

    done <3

--- resources/views/anime/show.blade.php
+++ resources/views/anime/show.blade.php
@@ -1,119 +1,99 @@
 @extends('template.navbar')
 
 @section('title')
-{{ $anime->anime_name }} - Anime
+  {{ $anime->anime_name }} - Anime
 @endsection
 
 @section('content')
-<div
-  class="title-section text-white bg-grayMid px-[12px] py-[5px] text-[18px] border-b-[1px] border-borderLight flex justify-between">
-  {{ $anime->anime_name }}
-  <a class="text-customBlueLight hover:underline" href="{{ route('anime.update', $anime->id) }}">
-    Edit
-  </a>
-</div>
-
-<div class="container bg-customBlack max-w-full border-[1px] border-border px-[15px] py-[10px] flex w-full">
-
-  <div class="left-section pr-[5px] h-full flex flex-col max-w-[250px]">
-    <img src="{{ Storage::url($anime->image_url) }}" alt="{{ $anime->anime_name }}" class="w-[250px] mb-[10px]">
-    <div class="container flex p-[15px] gap-[10px] justify-between bg-grayDark border-[1px] border-border ">
-      <div class="rating text-white flex flex-col items-center">
-        <div class="text-white bg-customBlue w-full text-center px-[15px] rounded-sm">Score</div>
-        <p class="text-[25px]">{{ $anime->mal_score }}</p>
-      </div>
-      <div class="view text-white">
-        <div class="bg-customBlue w-full text-center px-[15px] rounded-sm">View</div>
-        <p class="text-[25px]">{{ number_format($anime->views) }}</p>
-      </div>
-    </div>
-    <div class="information-section mb-[10px]">
-      <div class="title text-white border-b-[1px] border-borderLight font-bold text-[16px] my-[10px]">Information</div>
-      <div class="content w-full flex flex-col text-white">
-        <p><b class="text-gray-300">Name: </b>{{ $anime->anime_name }}</p>
-        <p><b class="text-gray-300">Relesae date: </b>{{ $anime->release_date }}</p>
-        <p><b class="text-gray-300">Episodes: </b>{{ $anime->total_episodes }}</p>
-        <p><b class="text-gray-300">Studios: </b><a class="text-customBlueLight hover:underline"
-            href="{{ route('studio.show', $anime->studio->id) }}">
-            {{ $anime->studio->studio_name }}
-          </a></p>
-        <p><b class="text-gray-300">Genres: </b>
-          @foreach ($anime->genres as $genre)
-          <a class="text-customBlueLight hover:underline" href="{{ route('genre.show', $genre->id) }}"
-            class="text-blue-500">{{ $genre->genre_name }}</a>@if (!$loop->last),@endif
-          @endforeach
-        </p>
-        <p><b class="text-gray-300:">Season: </b>
-          <a class="text-customBlueLight hover:underline" href="{{route('season.show', $anime->season->id)}}">
-            {{$anime->season->season_name}}
-          </a>
-        </p>
-      </div>
-    </div>
-    <div class="statistic-section mb-[10px]">
-      <div class="title text-white border-b-[1px] border-borderLight font-bold text-[16px] my-[10px]">Statistic</div>
-      <div class="content w-full flex flex-col text-white">
-        <p><b class="text-gray-300">Scores: {{ $anime->mal_score }}</p>
-        <p><b class="text-gray-300">Views: {{ number_format($anime->views) }}</p>
-      </div>
-    </div>
-  </div>
-  <div class="right-section flex-1 flex flex-col gap-[15px] pl-[5px] border-l-[1px] border-border">
-    <div class="synopsis-section">
-      <div class="title text-white border-b-[1px] border-borderLight font-bold text-[16px] mb-[10px]">Synopsis</div>
-      <div class="content text-gray-300">
-        {{ $anime->synopsis }}
-      </div>
-    </div>
-    <div class="trailer-section">
-      <div class="title text-white border-b-[1px] border-borderLight font-bold text-[16px] my-[10px]">Trailer</div>
-      <div class="content w-full bg-black">
-        <iframe class="w-4/5 mx-auto h-[340px]" src="https://www.youtube.com/embed/{{ $anime->trailer_url }}"
-          frameborder="0" allowfullscreen></iframe>
-      </div>
-    </div>
-    <div class="character-section">
-      <div class="title text-white border-b-[1px] border-borderLight font-bold text-[16px]">Characters &
-        Voice Actors</div>
-      <div class="content w-ful grid grid-cols-2">
-        @foreach ($anime->characters as $character)
-        <div class="container text-[13px] flex justify-between pt-[5px] pb-[5px] border-[1px] border-border">
-          <div class="left flex gap-[10px]">
-            <img class="w-[44px]" src={{ $character->character_image }} alt="">
-            <a class="text-customBlueLight hover:underline" href="">{{ $character->character_name }}</a>
-          </div>
-          <div class="right flex gap-[10px]">
-            <a class="text-customBlueLight hover:underline"
-              href="{{ route('voice_actor.show', $character->voiceActor->id) }}">{{
-              $character->voiceActor->voice_actor_name }}</a>
-            <img class="w-[44px]" src={{ $character->voiceActor->voice_actor_image }} alt="">
-          </div>
-        </div>
-        @endforeach
-      </div>
-    </div>
-    {{-- <h1 class="text-2xl font-bold">{{ $anime->anime_name }}</h1>
-    <p class="text-gray-300">Release Date: {{ $anime->release_date }}</p>
-    <p class="text-gray-300">Views: {{ $anime->views }}</p>
-    <p class="text-gray-300">MAL Score: {{ $anime->mal_score }}</p>
-    <p class="text-gray-300">{{ $anime->synopsis }}</p>
-    <a href="{{route('studio.show', $anime->studio->id)}}">
-      {{$anime->studio->studio_name}}
+  <div
+    class="title-section text-white bg-grayMid px-[12px] py-[5px] text-[18px] border-b-[1px] border-borderLight flex justify-between">
+    {{ $anime->anime_name }}
+    <a class="text-customBlueLight hover:underline" href="{{ route('anime.update', $anime->id) }}">
+      Edit
     </a>
-    <div class="video-container">
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/{{ $anime->trailer_url }}" frameborder="0"
-        allowfullscreen></iframe>
-    </div>
-    @foreach ($anime->characters as $character)
-    <p>{{$character->character_name}}
-      <a href="{{ route('voice_actor.show', $character->voiceActor->id)}}">
-        {{ $character->voiceActor->voice_actor_name }}
-      </a>
-    </p>
-    @endforeach
-    @foreach ($anime->genres as $genre)
-    <a href="{{ route('genre.show', $genre->id) }}" class="text-blue-500">{{ $genre->genre_name }}</a>
-    @endforeach --}}
   </div>
-</div>
+
+  <div class="container bg-customBlack max-w-full border-[1px] border-border px-[15px] py-[10px] flex w-full">
+
+    <div class="left-section pr-[5px] h-full flex flex-col max-w-[250px]">
+      <img src="{{ Storage::url($anime->image_url) }}" alt="{{ $anime->anime_name }}" class="w-[250px] mb-[10px]">
+      <div class="container flex p-[15px] gap-[10px] justify-between bg-grayDark border-[1px] border-border ">
+        <div class="rating text-white flex flex-col items-center">
+          <div class="text-white bg-customBlue w-full text-center px-[15px] rounded-sm">Score</div>
+          <p class="text-[25px]">{{ $anime->mal_score }}</p>
+        </div>
+        <div class="view text-white">
+          <div class="bg-customBlue w-full text-center px-[15px] rounded-sm">View</div>
+          <p class="text-[25px]">{{ number_format($anime->views) }}</p>
+        </div>
+      </div>
+      <div class="information-section mb-[10px]">
+        <div class="title text-white border-b-[1px] border-borderLight font-bold text-[16px] my-[10px]">Information</div>
+        <div class="content w-full flex flex-col text-white">
+          <p><b class="text-gray-300">Name: </b>{{ $anime->anime_name }}</p>
+          <p><b class="text-gray-300">Relesae date: </b>{{ $anime->release_date }}</p>
+          <p><b class="text-gray-300">Episodes: </b>{{ $anime->total_episodes }}</p>
+          <p><b class="text-gray-300">Studios: </b><a class="text-customBlueLight hover:underline"
+              href="{{ route('studio.show', $anime->studio->id) }}">
+              {{ $anime->studio->studio_name }}
+            </a></p>
+          <p><b class="text-gray-300">Genres: </b>
+            @foreach ($anime->genres as $genre)
+              <a class="text-customBlueLight hover:underline" href="{{ route('genre.show', $genre->id) }}"
+                class="text-blue-500">{{ $genre->genre_name }}</a>
+              @if (!$loop->last)
+                ,
+              @endif
+            @endforeach
+          </p>
+          <p><b class="text-gray-300:">Season: </b>
+            <a class="text-customBlueLight hover:underline" href="{{ route('season.show', $anime->season->id) }}">
+              {{ $anime->season->season_name }}
+            </a>
+          </p>
+        </div>
+      </div>
+      <div class="statistic-section mb-[10px]">
+        <div class="title text-white border-b-[1px] border-borderLight font-bold text-[16px] my-[10px]">Statistic</div>
+        <div class="content w-full flex flex-col text-white">
+          <p><b class="text-gray-300">Scores: {{ $anime->mal_score }}</p>
+          <p><b class="text-gray-300">Views: {{ number_format($anime->views) }}</p>
+        </div>
+      </div>
+    </div>
+    <div class="right-section flex-1 flex flex-col gap-[15px] pl-[5px] border-l-[1px] border-border">
+      <div class="synopsis-section">
+        <div class="title text-white border-b-[1px] border-borderLight font-bold text-[16px] mb-[10px]">Synopsis</div>
+        <div class="content text-gray-300">
+          {{ $anime->synopsis }}
+        </div>
+      </div>
+      <div class="trailer-section">
+        <div class="title text-white border-b-[1px] border-borderLight font-bold text-[16px] my-[10px]">Trailer</div>
+        <div class="content w-full bg-black">
+          <iframe class="w-4/5 mx-auto h-[340px]" src="https://www.youtube.com/embed/{{ $anime->trailer_url }}"
+            frameborder="0" allowfullscreen></iframe>
+        </div>
+      </div>
+      <div class="character-section">
+        <div class="title text-white border-b-[1px] border-borderLight font-bold text-[16px]">Characters &
+          Voice Actors</div>
+        <div class="content w-ful grid grid-cols-2">
+          @foreach ($anime->characters as $character)
+            <div class="container text-[13px] flex justify-between pt-[5px] pb-[5px] border-[1px] border-border">
+              <div class="left flex gap-[10px]">
+                <img class="w-[44px]" src={{ $character->character_image }} alt="">
+                <p class="text-white">{{ $character->character_name }}</p>
+              </div>
+              <div class="right flex gap-[10px]">
+                <a class="text-customBlueLight hover:underline"
+                  href="{{ route('voice_actor.show', $character->voiceActor->id) }}">{{ $character->voiceActor->voice_actor_name }}</a>
+                <img class="w-[44px]" src={{ $character->voiceActor->voice_actor_image }} alt="">
+              </div>
+            </div>
+          @endforeach
+        </div>
+      </div>
+    </div>
+  </div>
 @endsection
--- resources/views/template/navbar.blade.php
+++ resources/views/template/navbar.blade.php
@@ -21,7 +21,7 @@
   </style>
 </head>
 
-<body class="bg-customBlack flex justify-center items-center flex-col">
+<body class="bg-customBlack flex justify-center items-center flex-col overflow-x-hidden">
   <div class="main flex flex-col">
 
     {{-- Navigation Bar --}}
@@ -30,10 +30,6 @@
         <a href="{{ route('home') }}" class="w-2/12 h-auto">
           <img src="{{ asset('/image/VeKuZhuoList.png') }}" alt="">
         </a>
-        {{-- <div class="login-register-button flex justify-between center w-3/12 gap-2">
-          <a class="w-1/2 flex justify-center bg-grayLight font-bold text-white">Login</a>
-          <a class="w-1/2 flex justify-center bg-customBlue font-bold text-white">Sign Up</a>
-        </div> --}}
       </div>
       <div class="navbar-bottom bg-customBlue w-full flex flex-row justify-between h-[40px]">
         <div class="dropdown-section flex flex-row text-white font-bold">
@@ -41,11 +37,12 @@
             Anime
             <div
               class="dropdown-content absolute bg-grayLight whitespace-nowrap hidden flex-col left-0 top-full z-10 w-[150px] text-[12px]">
-              <a class="py-[10px] px-[15px] hover:bg-white hover:text-grayLight" href="{{route('anime.top_views')}}">Top
+              <a class="py-[10px] px-[15px] hover:bg-white hover:text-grayLight"
+                href="{{ route('anime.top_views') }}">Top
                 Viewed Anime</a>
               <a class="py-[10px] px-[15px] hover:bg-white hover:text-grayLight"
-                href="{{route('anime.top_malscore')}}">Top Rated Anime</a>
-              <a class="py-[10px] px-[15px] hover:bg-white hover:text-grayLight" href="{{route('anime.add')}}">Add
+                href="{{ route('anime.top_malscore') }}">Top Rated Anime</a>
+              <a class="py-[10px] px-[15px] hover:bg-white hover:text-grayLight" href="{{ route('anime.add') }}">Add
                 Anime</a>
             </div>
           </div>
@@ -53,9 +50,9 @@
             Studio
             <div
               class="dropdown-content absolute bg-grayLight whitespace-nowrap hidden flex-col left-0 top-full z-10 w-[150px] text-[12px] max-h-[300px] overflow-auto">
-              @foreach($studios as $studio)
-              <a class="py-[10px] px-[15px] hover:bg-white hover:text-grayLight"
-                href="{{ route('studio.show', $studio->id) }}">{{$studio->studio_name}}</a>
+              @foreach ($studios as $studio)
+                <a class="py-[10px] px-[15px] hover:bg-white hover:text-grayLight"
+                  href="{{ route('studio.show', $studio->id) }}">{{ $studio->studio_name }}</a>
               @endforeach
             </div>
           </div>
@@ -63,9 +60,9 @@
             Genre
             <div
               class="dropdown-content absolute bg-grayLight whitespace-nowrap hidden flex-col left-0 top-full z-10 w-[150px] text-[12px] max-h-[300px] overflow-auto">
-              @foreach($genres as $genre)
-              <a class="py-[10px] px-[15px] hover:bg-white hover:text-grayLight"
-                href="{{ route('genre.show', $genre->id) }}">{{$genre->genre_name}}</a>
+              @foreach ($genres as $genre)
+                <a class="py-[10px] px-[15px] hover:bg-white hover:text-grayLight"
+                  href="{{ route('genre.show', $genre->id) }}">{{ $genre->genre_name }}</a>
               @endforeach
             </div>
           </div>
@@ -73,9 +70,9 @@
             Season
             <div
               class="dropdown-content absolute bg-grayLight whitespace-nowrap hidden flex-col left-0 top-full z-10 w-[150px] text-[12px] max-h-[300px] overflow-auto">
-              @foreach($seasons as $season)
-              <a class="py-[10px] px-[15px] hover:bg-white hover:text-grayLight"
-                href="{{ route('season.show', $season->id) }}">{{$season->season_name}}</a>
+              @foreach ($seasons as $season)
+                <a class="py-[10px] px-[15px] hover:bg-white hover:text-grayLight"
+                  href="{{ route('season.show', $season->id) }}">{{ $season->season_name }}</a>
               @endforeach
             </div>
           </div>
@@ -96,21 +93,23 @@
     <div class="content">
       @yield('content')
     </div>
+
   </div>
+
   {{-- Footer --}}
-  <div class="footer bg-grayMid w-full mt-[30px] p-[50px] text-lightFont flex flex-col items-center justify-center">
+  <div class="footer bg-grayMid w-screen mt-[30px] p-[50px] text-lightFont flex flex-col items-center justify-center">
     <div class="top flex justify-center items-center gap-[20px] pb-[15px]">
       <p class="text-[11px]">Follow Us</p>
       <div class="link flex gap-[10px]">
         <a target="_blank" class="saturate-0 hover:saturate-100" href="https://www.instagram.com/jmaverick_/">
           <div class="instagram flex flex-col items-center gap-[2px] relative">
-            <img class="w-[30px]" src="{{ Storage::url('./instagram_logo.webp')}}" alt="">
+            <img class="w-[30px]" src="{{ Storage::url('./instagram_logo.webp') }}" alt="">
             <p class="tag bg-grayDark absolute text-[10px] bottom-full p-[5px] rounded-md hidden">@jmaverick_</p>
           </div>
         </a>
         <a target="_blank" class="saturate-0 hover:saturate-100" href="https://www.instagram.com/hendrazhuoh/">
           <div class="instagram flex flex-col items-center gap-[2px] relative">
-            <img class="w-[30px]" src="{{ Storage::url('./instagram_logo.webp')}}" alt="">
+            <img class="w-[30px]" src="{{ Storage::url('./instagram_logo.webp') }}" alt="">
             <p class="tag bg-grayDark absolute text-[10px] bottom-full p-[5px] rounded-md hidden">@hendrazhuoh</p>
           </div>
         </a>
@@ -123,6 +122,7 @@
         apply.</p>
     </div>
   </div>
+
 </body>
 
 </html>
