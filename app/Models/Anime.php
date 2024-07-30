719dc8fe496cfaf1f2058770c7e6da0760210ba3
Jonathan Maverick <jonathanmaverick.jm@gmail.com>
2024-07-30 13:04:12 +0700

    gitignore for storage:

--- app/Models/Anime.php
+++ app/Models/Anime.php
@@ -7,38 +7,38 @@ use Illuminate\Database\Eloquent\Model;
 
 class Anime extends Model
 {
-  protected $fillable = [
-    'anime_name',
-    'release_date',
-    'views',
-    'mal_score',
-    'synopsis',
-    'trailer_url',
-    'image_url',
-    'total_episodes',
-    'studio_id',
-    'season_id', 
+    protected $fillable = [
+        'anime_name',
+        'release_date',
+        'views',
+        'mal_score',
+        'synopsis',
+        'trailer_url',
+        'image_url',
+        'total_episodes',
+        'studio_id',
+        'season_id',
     ];
 
-  public function genres()
-  {
-    return $this->belongsToMany(Genre::class, 'anime_genres');
-  }
+    public function genres()
+    {
+        return $this->belongsToMany(Genre::class, 'anime_genres');
+    }
 
-  public function studio()
-  {
-    return $this->belongsTo(Studio::class);
-  }
+    public function studio()
+    {
+        return $this->belongsTo(Studio::class);
+    }
 
-  public function season()
-  {
-    return $this->belongsTo(Season::class);
-  }
+    public function season()
+    {
+        return $this->belongsTo(Season::class);
+    }
 
-  public function characters()
-  {
-    return $this->hasMany(Character::class);
-  }
+    public function characters()
+    {
+        return $this->hasMany(Character::class);
+    }
 
-  use HasFactory;
+    use HasFactory;
 }
