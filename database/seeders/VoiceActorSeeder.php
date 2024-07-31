422727b908a0a10fc82ffa2ebb71c17ba0b14933
Jonathan Maverick <jonathanmaverick.jm@gmail.com>
2024-07-31 12:45:50 +0700

    test

--- database/seeders/VoiceActorSeeder.php
+++ database/seeders/VoiceActorSeeder.php
@@ -312,6 +312,10 @@ class VoiceActorSeeder extends Seeder
       [
         'voice_actor_name' => 'Ryouta Suzuki',
         'voice_actor_image' => 'https://cdn.myanimelist.net/images/voiceactors/2/64211.jpg'
+      ],
+      [
+        'voice_actor_name' => 'Nao Taoyama',
+        'voice_actor_image' => 'https://cdn.myanimelist.net/images/voiceactors/3/80857.jpg'
       ]
     ];
 
